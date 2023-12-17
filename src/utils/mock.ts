5a113f73c0d57272dfa824a0722283c393ba09d8
Elvis Serafim <elvis.serafim@deliverit.com.br>
2023-12-17 04:16:29 -0300

    feat: create mock for simulate database

---
+++ src/utils/mock.ts
@@ -0,0 +1,10 @@
+import { Contact } from './types';
+
+export const contacts: Contact[] = [
+  {
+    id: '3def4b69-01ab-46e6-9fbb-a26af001478e',
+    firstName: 'Elvis',
+    lastName: 'Serafim',
+    phoneNumber: '759-888-71979',
+  },
+];
