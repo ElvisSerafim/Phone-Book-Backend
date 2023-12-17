0473c773d7c007707094ab08eb1f611ac4b0e4a3
Elvis Serafim <elvis.serafim@deliverit.com.br>
2023-12-17 11:05:07 -0300

    feat: update phone number mock

--- src/utils/mock.ts
+++ src/utils/mock.ts
@@ -5,6 +5,6 @@ export const contacts: Contact[] = [
     id: '3def4b69-01ab-46e6-9fbb-a26af001478e',
     firstName: 'Elvis',
     lastName: 'Serafim',
-    phoneNumber: '759-888-71979',
+    phoneNumber: '759-888-7197',
   },
 ];
